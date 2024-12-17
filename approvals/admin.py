1c0496ec988b91ae7a6a6374c7125aafa2a9430e
Chris Hancock <chris@rejigg.com>
2024-12-17 15:54:11 -0800

    refactor: Admin uses agent registry

--- approvals/admin.py
+++ approvals/admin.py
@@ -5,14 +5,15 @@ from pprint import pformat
 load_dotenv()
 
 from django.contrib import admin
+from django import forms
 from .models import Approval, Trigger
 
-from approvals.agent import get_agent
+from approvals.agent import get_agent, agent_registry
 
 
 @admin.register(Approval)
 class ApprovalAdmin(admin.ModelAdmin):
-    list_display = ("state", "response", "user", "created_at", "updated_at", "render_history")
+    list_display = ("id", "agent_name", "state", "response", "user", "created_at", "updated_at")
     list_filter = ("state", "created_at", "updated_at")
     search_fields = ("user__username", "comment")
     readonly_fields = ("render_history",)
@@ -27,7 +28,16 @@ class ApprovalAdmin(admin.ModelAdmin):
     render_history.short_description = "SnapshotHistory"
 
 
+class TriggerForm(forms.ModelForm):
+    class Meta:
+        model = Trigger
+        fields = "__all__"
+
+    agent_name = forms.ChoiceField(choices=[(key, key) for key in agent_registry.keys()])
+
+
 @admin.register(Trigger)
 class TriggerAdmin(admin.ModelAdmin):
+    form = TriggerForm
     list_display = ("agent_name", "config", "input")
     search_fields = ("agent_name", "config")
